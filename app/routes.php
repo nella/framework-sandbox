2c20f36877ee37f8b1dad4737c3bdd4290bce490
Patrik Votocek <patrik@votocek.cz>
2011-04-04 20:41:10 +0200

    Add media routes

--- app/routes.php
+++ app/routes.php
@@ -10,5 +10,15 @@ use Nette\Application\Route;
 
 $router = Nette\Environment::getApplication()->getRouter();
 
+// Homepage
 $router[] = new Route("index.php", "Homepage:default", Route::ONE_WAY);
+
+
+// Media
+$route = new \Nella\Media\FileRoute('<file>', "Media:Media:file");
+$router[] = $route->setEntityManager($em);
+$route = new \Nella\Media\ImageRoute('images/<format>/<image>.<type>', "Media:Media:image");
+$router[] = $route->setEntityManager($em);
+
+// Default
 $router[] = new Route("<presenter>/<action>[/<id>]", "Homepage:default");
