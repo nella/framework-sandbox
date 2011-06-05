e1ede59ddcd4c3a31057975eedf681d22f301fbe
Patrik Votocek <patrik@votocek.cz>
2011-06-06 01:36:26 +0200

    update js libs

--- www/js/nellaMfu.js
+++ www/js/nellaMfu.js
@@ -10,42 +10,42 @@ jQuery.fn.nellaMultipleFileUploader = function(options) {
 	if (!window.FileReader || !window.FormData) {
 		return false;
 	}
-	
+
 	options = jQuery.extend({
 		templates: {
-			queue: '<div class="nella-file-upload-queue">', 
-			queueItem: '<div class="item">', 
-			queueItemImg: '<img alt="item image">', 
-			removeItemButton: '<a href="#" class="remove-button"><span>remove</span></a>'
+			queue: '<div class="nella-file-upload-queue">',
+			queueItem: '<div class="item">',
+			queueItemImg: '<img alt="item image">',
+			removeItemButton: '<a href="#" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-icon-only ui-state-focus remove-button" role="button" aria-disabled="false" title="remove"><span class="ui-button-icon-primary ui-icon ui-icon-trash"></span><span class="ui-button-text">remove</span></a>'
 		},
 		thumbnail: {
 			width: 128,
 			height: 128
-		}, 
-		token: 'xxx', 
-		image: false, 
-		notImageMessage: 'Only for images', 
+		},
+		token: 'xxx',
+		image: false,
+		notImageMessage: 'Only for images',
 		submit: null
 	}, options);
-	
+
 	this.each(function(queueIndex) {
 		if (!$(this).is('input[type=file][multiple]')) {
 			return false;
 		}
-		
+
 		$(this).change(function() {
 			var $queue = $(options.templates.queue);
 			var $this = $(this);
-			
+
 			for (var i in this.files) {
 				var file = this.files[i];
 				var alerted = false;
-				
+
 				if (file instanceof File && isImage(file)) {
 					readfile(file, function(file, content) {
 						$(options.templates.queueItemImg).attr('src', content).load(function() {
 							resizeImage(this, options.thumbnail.width, options.thumbnail.height);
-							
+
 							$item = $(options.templates.queueItem).append(
 								$(options.templates.removeItemButton).click(function() {
 									$(this).parents('.nella-file-upload-queue .item').remove();
@@ -53,7 +53,7 @@ jQuery.fn.nellaMultipleFileUploader = function(options) {
 								})
 							).append($(this)).hide();
 							$item[0].file = file;
-							
+
 							$queue.append($item);
 							$item.show('fast');
 						});
@@ -66,7 +66,7 @@ jQuery.fn.nellaMultipleFileUploader = function(options) {
 						})
 					).append($('<p>').text(file.name));
 					$item[0].file = file;
-					
+
 					$queue.append($item);
 					$item.show('fast');
 				} else if (file instanceof File && !alerted) {
@@ -74,7 +74,7 @@ jQuery.fn.nellaMultipleFileUploader = function(options) {
 					alerted = true;
 				}
 			}
-			
+
 			var $actualQueue = $('#' + options.templates.queueIdPrefix + queueIndex);
 			if ($actualQueue.get(0) != undefined) {
 				$actualQueue.replaceWith($queue);
@@ -82,7 +82,7 @@ jQuery.fn.nellaMultipleFileUploader = function(options) {
 				$(this).before($queue);
 			}
 		});
-		
+
 		$(this).parents('form:last').submit(function() {
 			$(this).find('.nella-file-upload-queue').each(function() {
 				$(this).find('.item').each(function() {
@@ -90,28 +90,28 @@ jQuery.fn.nellaMultipleFileUploader = function(options) {
 					$form = $item.parents('form');
 					var fd = new FormData();
 					fd.append('file', this.file);
-					
+
 					$item.find('.remove-button').remove();
 					$item.append($('<div class="progressbar">').progressbar({value: 0}));
 					$item.parents('.nella-file-upload-queue').next('input[type=file]').remove();
-					
+
 					$.ajax({
-						processData : false, 
-						url: $form.attr('action'), 
-						contentType: false, 
-						type: $form.attr('method') || 'get', 
-						data: fd, 
-						context: $item, 
+						processData : false,
+						url: $form.attr('action'),
+						contentType: false,
+						type: $form.attr('method') || 'get',
+						data: fd,
+						context: $item,
 						headers: {
-							'X-Uploader': 'Nella Framework - MFU', 
+							'X-Uploader': 'Nella Framework - MFU',
 							'X-Nella-MFU-Token': options.token
-						}, 
+						},
 						success: function() {
 							$(this).hide('slow', function() {
 								$this = $(this);
 								$form = $this.parents('form');
 								$this.remove();
-								
+
 								if ($form.find('.nella-file-upload-queue .item')[0] == undefined) {
 									if (options.submit) {
 										options.submit($form);
@@ -119,8 +119,8 @@ jQuery.fn.nellaMultipleFileUploader = function(options) {
 										$form[0].submit();
 									}
 								}
-							}); 
-						}, 
+							});
+						},
 						progress: function(jqXHR, event) {
 							this.context.find('.progressbar').progressbar({
 								value: parseInt((event.total / event.loaded) * 100)
@@ -129,27 +129,27 @@ jQuery.fn.nellaMultipleFileUploader = function(options) {
 					});
 				});
 			});
-			
+
 			return false;
 		});
 	});
-	
-	
+
+
 	function resizeImage(image, height, width)
 	{
 		var	actualSize = {
-			width: image.naturalWidth, 
+			width: image.naturalWidth,
 			height: image.naturalHeight
 		};
 		var newSize = {
-			width: null, 
+			width: null,
 			height: null
 		};
-		
+
 		if ((actualSize.width / width) <= (actualSize.height / height)) {
 			newSize.height = (actualSize.height > height) ? height : actualSize.height;
 			newSize.width = parseInt((newSize.height * actualSize.width) / actualSize.height);
-			
+
 		}
 		else {
 			newSize.width = (actualSize.width > width) ? width : actualSize.width;
@@ -159,7 +159,7 @@ jQuery.fn.nellaMultipleFileUploader = function(options) {
 		image.width = newSize.width;
 		image.height = newSize.height;
 	}
-	
+
 	function isImage(file, allowedImageTypes) {
 		allowedImageTypes = allowedImageTypes || ['image/jpg', 'image/gif', 'image/png', 'image/jpeg'];
 		for (var i in allowedImageTypes) {
@@ -170,14 +170,14 @@ jQuery.fn.nellaMultipleFileUploader = function(options) {
 
 		return false;
 	}
-	
+
 	function readfile(file, callback) {
 		var reader = new FileReader();
 		reader.onload = function(e) {
 			callback(file, e.target.result);
 		};
 		reader.readAsDataURL(file);
-		
+
 		reader.onerror = function(e) {
 			console.log(e);
 		};
@@ -187,7 +187,7 @@ jQuery.fn.nellaMultipleFileUploader = function(options) {
 /*!
  * jQuery ajaxProgress Plugin v0.5.0
  * Requires jQuery v1.5.0 or later
- * 
+ *
  * http://www.kpozin.net/ajaxprogress
  *
  * (c) 2011, Konstantin Pozin
